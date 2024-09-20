f439b49fff13cc67c7c2c2cafed400999adf0727
dDevusS <140493120+dDevusS@users.noreply.github.com>
2024-09-20 15:53:10 -0400

    refactor: delete overwrited method equels() from Coordinate due to Coordinate is record class with right realization of this method in default

--- src/resources/Coordinate.java
+++ src/resources/Coordinate.java
@@ -10,15 +10,4 @@ public record Coordinate(int x, int y) {
 		return new Coordinate(this.x + rateShiftX, this.y + rateShiftY);
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Coordinate other = (Coordinate) obj;
-		return x == other.x && y == other.y;
-	}
 }
